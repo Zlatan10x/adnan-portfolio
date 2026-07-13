b155d40366f544d070b8f6ea2a574441fba9b9d8
Zlatan10x <adnanmohammad546@gmail.com>
2026-07-13 20:06:16 +0600

    Create project structure and Hero component

---
+++ src/components/sections/Hero.tsx
@@ -0,0 +1,11 @@
+export default function Hero() {
+  return (
+    <section>
+      <h1>Hello, I'm Adnan.</h1>
+
+      <p>
+        Computer Science Student • AI Automation Builder • Software Developer
+      </p>
+    </section>
+  );
+}
